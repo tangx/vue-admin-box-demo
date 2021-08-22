522ceb2ba7e95bb25c75de67221486ad37c766a5
tangx <uyinn@live.com>
2021-08-22 09:30:51 +0800

    docs: import readonly in es6

--- src/global/index.ts
+++ src/global/index.ts
@@ -12,7 +12,9 @@ const config = reactive(
 )
 export default config
 
-
+// import的是export的只读引用，第二种只是改变了test的属性值，这个属性并不是只读的
+// https://segmentfault.com/q/1010000011607153
+// https://exploringjs.com/es6/ch_modules.html#_imports-are-read-only-views-on-exports
 export function initialConfig() {
     config.GITHUB_API = 'https://api.github.com'
 }
